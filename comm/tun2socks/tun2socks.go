922984af5e53a1c81179251268ac402adee8afb5
dosgo <dosgo@qq.com>
2022-02-16 12:13:18 +0800

    Update tun2socks.go

--- comm/tun2socks/tun2socks.go
+++ comm/tun2socks/tun2socks.go
@@ -2,20 +2,21 @@ package tun2socks
 
 import (
 	"bytes"
+	"context"
 	"goSocksTap/comm/netstack"
+	"io"
+	"log"
+
 	"gvisor.dev/gvisor/pkg/tcpip/buffer"
 	"gvisor.dev/gvisor/pkg/tcpip/header"
 	"gvisor.dev/gvisor/pkg/tcpip/stack"
-	"io"
-	"log"
-	"context"
 )
 
-func ForwardTransportFromIo(dev io.ReadWriteCloser,mtu int,tcpCallback netstack.ForwarderCall,udpCallback netstack.UdpForwarderCall) error {
-	_,channelLinkID,err:=netstack.NewDefaultStack(mtu,tcpCallback,udpCallback);
-	if err!=nil {
-		log.Printf("err:%v",err)
-		return err;
+func ForwardTransportFromIo(dev io.ReadWriteCloser, mtu int, tcpCallback netstack.ForwarderCall, udpCallback netstack.UdpForwarderCall) error {
+	_, channelLinkID, err := netstack.NewDefaultStack(mtu, tcpCallback, udpCallback)
+	if err != nil {
+		log.Printf("err:%v", err)
+		return err
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -23,39 +24,42 @@ func ForwardTransportFromIo(dev io.ReadWriteCloser,mtu int,tcpCallback netstack.
 
 	// write tun
 	go func(_ctx context.Context) {
-		var sendBuffer =new(bytes.Buffer)
+		var sendBuffer = new(bytes.Buffer)
 		for {
-			info,res:=channelLinkID.ReadContext(_ctx)
-			if !res {
+			info := channelLinkID.ReadContext(_ctx)
+			if info == nil {
 				log.Printf("channelLinkID exit \r\n")
-				break;
+				break
 			}
 			sendBuffer.Reset()
 			//buffer.Write(pkt.Pkt.LinkHeader().View())
-			sendBuffer.Write(info.Pkt.NetworkHeader().View())
-			sendBuffer.Write(info.Pkt.TransportHeader().View())
-			sendBuffer.Write(info.Pkt.Data().AsRange().ToOwnedView())
-			if sendBuffer.Len()>0 {
+			sendBuffer.Write(info.NetworkHeader().View())
+			sendBuffer.Write(info.TransportHeader().View())
+			sendBuffer.Write(info.Data().AsRange().ToOwnedView())
+			if sendBuffer.Len() > 0 {
 				dev.Write(sendBuffer.Bytes())
 			}
 		}
 	}(ctx)
 
-
 	// read tun data
-	var buf=make([]byte,mtu+80)
+	var buf = make([]byte, mtu+80)
 	for {
 		n, e := dev.Read(buf[:])
 		if e != nil {
-			log.Printf("err:%v",err)
-			break;
+			log.Printf("err:%v", err)
+			break
 		}
-		tmpView:=buffer.NewVectorisedView(n,[]buffer.View{
-			buffer.NewViewFromBytes(buf[:n]),
+		pkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
+			Data: buffer.View(buf[:n]).ToVectorisedView(),
 		})
-		channelLinkID.InjectInbound(header.IPv4ProtocolNumber, stack.NewPacketBuffer(stack.PacketBufferOptions{
-			Data: tmpView,
-		}))
+		switch header.IPVersion(buf[:]) {
+		case header.IPv4Version:
+			channelLinkID.InjectInbound(header.IPv4ProtocolNumber, pkt)
+		case header.IPv6Version:
+			channelLinkID.InjectInbound(header.IPv6ProtocolNumber, pkt)
+		}
+		pkt.DecRef()
 	}
 	return nil
 }
