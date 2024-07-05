977b1b0b86f138331226feb04abdde8562fa30a5
dosgo <dosgo@qq.com>
2024-07-05 16:00:34 +0800

    Update winDivert.go

--- winDivert/winDivert.go
+++ winDivert/winDivert.go
@@ -8,7 +8,9 @@ import (
 	"encoding/binary"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"syscall"
 	"os"
 	"strconv"
 	"sync"
@@ -46,11 +48,7 @@ func RedirectDNS(dnsAddr string, _port string, sendStartPort int, sendEndPort in
 		return
 	}
 	winDivertRun = true
-	var filter = "outbound and !loopback and !impostor and udp.DstPort=53"
-
-	for i := sendStartPort; i <= sendEndPort; i++ {
-		filter = filter + " and udp.SrcPort!=" + strconv.Itoa(i)
-	}
+	var filter = "outbound and !loopback and !impostor and udp.DstPort=53  and (udp.SrcPort>"+strconv.Itoa(sendStartPort)+" or udp.SrcPort<"+strconv.Itoa(sendEndPort)+")"
 
 	winDivert, err = divert.Open(filter, divert.LayerNetwork, divert.PriorityDefault, divert.FlagDefault)
 	if err != nil {
@@ -159,9 +157,26 @@ func RedirectDNSV1(dnsAddr string, _port string) {
 			log.Println(1, err)
 			continue
 		}
+		var ipheadlen int
+		ipv6 := recvBuf[0]>>4 == 6
+		if ipv6 {
+			ipheadlen = 40
+		} else {
+			ipheadlen = int(recvBuf[0]&0xF) * 4
+		}
+	
+		//如果是本机的请求直接发送
+		srcPort := binary.BigEndian.Uint16(recvBuf[ipheadlen : ipheadlen+2])
+		_, ok := excludeOriginalAddr.Load("udp:" + strconv.Itoa(int(srcPort)))
+		log.Printf("srcPort:%d  ok:%v \r\n", srcPort, ok)
+		if ok {
+			winDivert.Send(recvBuf[:recvLen], &addr)
+			continue;
+		}
+
 
 		hash := fmt.Sprintf("%x", recvBuf[22:recvLen])
-		_, ok := removeRepeat.Load(hash)
+		_, ok = removeRepeat.Load(hash)
 		if !ok {
 			removeRepeat.Store(hash, 1)
 			go sendDns(dnsAddr, _port, recvBuf, recvLen, &addr, hash)
@@ -171,6 +186,118 @@ func RedirectDNSV1(dnsAddr string, _port string) {
 		time.Sleep(time.Millisecond * 50)
 	}
 
+}
+
+
+/*windows转发*/
+func RedirectDNSV3(dnsAddr string, _port string) {
+	var err error
+	_, err = os.Stat(divertDll)
+	if err != nil {
+		log.Printf("not found :%s\r\n", divertDll)
+		return
+	}
+	winDivertRun = true
+	var filter = "outbound and !loopback and !impostor and udp.DstPort=53 and (udp.SrcPort>700 or udp.SrcPort<600)"
+	winDivert, err = divert.Open(filter, divert.LayerNetwork, divert.PriorityDefault, divert.FlagDefault)
+	if err != nil {
+		log.Printf("winDivert open failed: %v\r\n", err)
+		return
+	}
+
+	rawbuf := make([]byte, 1500)
+	var dnsRecvBuf []byte = make([]byte, 1500)
+	var recvBuf []byte = make([]byte, 1500)
+	addr := divert.Address{}
+	var dnsConn net.Conn
+	dnsConn, _ = net.DialTimeout("udp", dnsAddr+":"+_port, time.Second*15)
+	var udpsize = 0
+	var packetsize int = 0
+	var ipheadlen int
+	var recvLen uint
+	var conRecvLen int
+	var udpheadlen = 8
+	for winDivertRun {
+		if winDivert == nil {
+			continue
+		}
+		recvLen, err = winDivert.Recv(recvBuf, &addr)
+		if err != nil {
+			log.Println(1, err)
+			continue
+		}
+		ipv6 := recvBuf[0]>>4 == 6
+		if ipv6 {
+			ipheadlen = 40
+		} else {
+			ipheadlen = int(recvBuf[0]&0xF) * 4
+		}
+
+	
+
+
+		dnsConn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+		dnsConn.Write(recvBuf[ipheadlen+udpheadlen : recvLen])
+		dnsConn.SetReadDeadline(time.Now().Add(10 * time.Second))
+		conRecvLen, err = dnsConn.Read(dnsRecvBuf)
+		if err == nil {
+			udpsize = conRecvLen + udpheadlen
+			if ipv6 {
+				copy(rawbuf, []byte{96, 12, 19, 68, 0, 98, 17, 128})
+				packetsize = 40 + udpsize
+				binary.BigEndian.PutUint16(rawbuf[4:], uint16(udpsize))
+				copy(rawbuf[8:], recvBuf[24:40])
+				copy(rawbuf[24:], recvBuf[8:24])
+				copy(rawbuf[ipheadlen:], recvBuf[ipheadlen+2:ipheadlen+4])
+				copy(rawbuf[ipheadlen+2:], recvBuf[ipheadlen:ipheadlen+2])
+			} else {
+				copy(rawbuf, []byte{69, 0, 1, 32, 141, 152, 64, 0, 64, 17, 150, 46})
+				packetsize = 20 + udpsize
+				binary.BigEndian.PutUint16(rawbuf[2:], uint16(packetsize))
+				copy(rawbuf[12:], recvBuf[16:20])
+				copy(rawbuf[16:], recvBuf[12:16])
+				copy(rawbuf[20:], recvBuf[ipheadlen+2:ipheadlen+4])
+				copy(rawbuf[22:], recvBuf[ipheadlen:ipheadlen+2])
+				ipheadlen = 20
+			}
+
+			binary.BigEndian.PutUint16(rawbuf[ipheadlen+4:], uint16(udpsize))
+			copy(rawbuf[ipheadlen+udpheadlen:], dnsRecvBuf[:conRecvLen])
+			divert.CalcChecksums(rawbuf[:packetsize], &addr, 0)
+			_, err = winDivert.Send(rawbuf[:packetsize], &addr)
+			if err != nil {
+				log.Println(1, err)
+				return
+			}
+		} else {
+			dnsConn.Close()
+			dnsConn, _ = net.DialTimeout("udp", dnsAddr+":"+_port, time.Second*15)
+		}
+	}
+	if dnsConn != nil {
+		dnsConn.Close()
+	}
+}
+
+func testV3(){
+	dialer := &net.Dialer{
+		Timeout:   10 * time.Second,
+		Control: func(network, address string, c syscall.RawConn) error {
+			var err error
+			for attempt := 0; attempt < 2; attempt++ { // 尝试最多两次
+				err = c.Control(func(fd uintptr) {
+					syscall.Bind(syscall.Handle(fd), &syscall.SockaddrInet4{
+						Port: rand.Intn(700 - 600) + 600, // 随机选择一个端口
+					})
+				})
+				if err == nil {
+					break
+				}
+			}
+			return err
+		},
+	}
+	dialer.Dial("udp","127.0.0.1:53")
 }
 
 func sendDns(dnsAddr string, _port string, recvBuf []byte, recvLen uint, addr *divert.Address, hash string) {
@@ -185,17 +312,6 @@ func sendDns(dnsAddr string, _port string, recvBuf []byte, recvLen uint, addr *d
 		ipheadlen = 40
 	} else {
 		ipheadlen = int(recvBuf[0]&0xF) * 4
-	}
-
-	//如果是本机的请求直接发送
-	srcPort := binary.BigEndian.Uint16(recvBuf[ipheadlen : ipheadlen+2])
-
-	_, ok := excludeOriginalAddr.Load("udp:" + strconv.Itoa(int(srcPort)))
-	log.Printf("srcPort:%d  ok:%v \r\n", srcPort, ok)
-
-	if ok {
-		winDivert.Send(recvBuf[:recvLen], addr)
-		return
 	}
 	rawbuf := make([]byte, 1500)
 	var udpsize = 0
@@ -351,4 +467,45 @@ func CloseWinDivert() {
 	if winDivertEvent != nil {
 		winDivertEvent.Close()
 	}
+}
+
+func Test3(){
+	var err error
+	_, err = os.Stat(divertDll)
+	if err != nil {
+		log.Printf("not found :%s\r\n", divertDll)
+		return
+	}
+	winDivertRun = true
+	var filter = "outbound and !loopback and !impostor and udp.DstPort=53 and (udp.SrcPort>700 or udp.SrcPort<600)"
+	winDivert, err = divert.Open(filter, divert.LayerNetwork, divert.PriorityDefault, divert.FlagDefault)
+	if err != nil {
+		log.Printf("winDivert open failed: %v\r\n", err)
+		return
+	}
+	var recvBuf []byte = make([]byte, 1500)
+	addr := divert.Address{}
+	for winDivertRun {
+		if winDivert == nil {
+			continue
+		}
+		recvLen, err := winDivert.Recv(recvBuf, &addr)
+		if err != nil {
+			log.Println(1, err)
+			continue
+		}
+		
+		var ipheadlen int
+		ipv6 := recvBuf[0]>>4 == 6
+		if ipv6 {
+			ipheadlen = 40
+		} else {
+			ipheadlen = int(recvBuf[0]&0xF) * 4
+		}
+	
+		//如果是本机的请求直接发送
+		srcPort := binary.BigEndian.Uint16(recvBuf[ipheadlen : ipheadlen+2])
+		fmt.Printf("srcPort:%d  recvBuf:%s\r\n",srcPort,string(recvBuf[:recvLen]));
+	}
+
 }
