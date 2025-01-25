e7ca4cfa047a67c36dc69d1ed8aee1a5929dcc9d
dosgo <dosgo@qq.com>
2025-01-25 11:48:58 +0800

    Update win.go

--- comm/netstat/win.go
+++ comm/netstat/win.go
@@ -52,3 +52,19 @@ func IsSocksServerAddr(pid int, addr string) bool {
 	}
 	return false
 }
+
+func GetUdpBindList(pid int, slefPid bool) ([]uint16, error) {
+	tbl, err := netstat.GetUDPTableOwnerPID(true)
+	if err != nil {
+		return nil, errors.New("error")
+	}
+	var _slefPid = os.Getpid()
+	var ports []uint16
+	s := tbl.Rows()
+	for i := range s {
+		if int(s[i].WinPid) == pid || int(s[i].WinPid) == _slefPid {
+			ports = append(ports, s[i].LocalSock().Port)
+		}
+	}
+	return ports, nil
+}
