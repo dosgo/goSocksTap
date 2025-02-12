032a902c545f1e0ee61ad3f5c0b65f90361af388
dosgo <dosgo@qq.com>
2025-02-12 10:06:20 +0800

    Update win.go

--- comm/netstat/win.go
+++ comm/netstat/win.go
@@ -13,7 +13,7 @@ import (
 )
 
 /*为啥要用这方法,因为Process在一些电脑比较耗时间只有匹配的才获取*/
-func PortGetPid(lSocks string) (int, error) {
+func PortGetPid(lSocks string) (uint32, error) {
 	socksAddrs := strings.Split(lSocks, ":")
 	lPort, err := strconv.Atoi(socksAddrs[1])
 	tbl, err := netstat.GetTCPTable2(true)
@@ -28,13 +28,13 @@ func PortGetPid(lSocks string) (int, error) {
 			State:      s[i].SockState(),
 		}
 		if ent.State == netstat.Listen && ent.LocalAddr.Port == uint16(lPort) {
-			return int(s[i].WinPid), nil
+			return uint32(s[i].WinPid), nil
 		}
 	}
 	return 0, nil
 }
 
-func IsSocksServerAddr(pid int, addr string) bool {
+func IsSocksServerAddr(pid uint32, addr string) bool {
 	tbl, err := netstat.GetTCPTable2(true)
 	if err != nil {
 		return false
@@ -47,7 +47,7 @@ func IsSocksServerAddr(pid int, addr string) bool {
 			State:      s[i].SockState(),
 		}
 		if strings.Index(ent.LocalAddr.String(), addr) != -1 {
-			if int(s[i].WinPid) == pid {
+			if uint32(s[i].WinPid) == pid {
 				return true
 			}
 		}
@@ -55,7 +55,7 @@ func IsSocksServerAddr(pid int, addr string) bool {
 	return false
 }
 
-func GetUdpBindList(pid int, slefPid bool) ([]uint16, error) {
+func GetUdpBindList(pid uint32, slefPid bool) ([]uint16, error) {
 	tbl, err := netstat.GetUDPTableOwnerPID(true)
 	if err != nil {
 		return nil, errors.New("error")
@@ -64,7 +64,7 @@ func GetUdpBindList(pid int, slefPid bool) ([]uint16, error) {
 	var ports []uint16
 	s := tbl.Rows()
 	for i := range s {
-		if int(s[i].WinPid) == pid || int(s[i].WinPid) == _slefPid {
+		if uint32(s[i].WinPid) == pid || int(s[i].WinPid) == _slefPid {
 			ports = append(ports, s[i].LocalSock().Port)
 		}
 	}
