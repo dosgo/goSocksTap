9565806fe4fa933338e18509716bc5cbf9510d54
dosgo <dosgo@qq.com>
2025-02-12 10:38:32 +0800

    Update nowinDivert.go

--- winDivert/nowinDivert.go
+++ winDivert/nowinDivert.go
@@ -3,6 +3,8 @@
 
 package winDivert
 
+import "github.com/dosgo/goSocksTap/tunDns"
+
 /*only windows*/
 func RedirectDNS(dnsAddr string, dnsPort uint16, sendStartPort int, sendEndPort int, localHost bool) {
 
@@ -15,5 +17,15 @@ func RedirectDNSV2(dnsAddr string, _port string, sendStartPort int, sendEndPort 
 
 /*only windows*/
 func CloseWinDivert() {
+
+}
+
+func NetEvent(pid uint32, tunDns *tunDns.TunDns) {
+
+}
+func HackDNSData(tunDns *tunDns.TunDns) {
+
+}
+func CloseNetEvent() {
 
 }
