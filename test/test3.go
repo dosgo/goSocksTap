43ea703cd79ab935a631b694415142d98b56d9c6
dosgo <dosgo@qq.com>
2026-01-21 17:22:42 +0800

    test

--- test/test3.go
+++ test/test3.go
@@ -3,7 +3,8 @@ package main
 import "github.com/dosgo/goSocksTap/socksTap"
 
 func main() {
-	socksTap := socksTap.NewSocksTap(1095, "127.0.0.1:10801", 0)
+	socksTap := socksTap.NewSocksTap(1095, "127.0.0.1:20170", 1)
 	socksTap.Start()
+	defer socksTap.Close()
 	select {}
 }
